5d98a6711c432ad76b707c3dbff60d686a2f9554
magas4891 <magasishe@gmail.com>
2019-07-18 11:26:10 +0300

    g controller Category

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,5 @@
+class CategoryController < ApplicationController
+  def index
+    @category = Category.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'category/index'
   get 'welcome/index'
   devise_for :users
+
   root 'welcome#index'
+
+  resources :category
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
