7dc90f4ed19666473cdcabd2d62e6a11df24c1b1
magas4891 <magasishe@gmail.com>
2019-07-31 13:36:10 +0300

    FB auth works 2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, "716204482167832", "0963b029ec134ae2d3ad94fe28b6075b", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, "App ID", "App Secret", callback_url: "http://localhost:3000/users/auth/facebook/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
