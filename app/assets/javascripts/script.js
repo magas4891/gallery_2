d2408e9c5bd29b6673879c07e5efd015bb346127
magas4891 <magasishe@gmail.com>
2019-08-30 12:29:06 +0300

    js nicescroll, sticky

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,19 @@
+/* JS for Plugins */
+
+$(document).ready(function(){
+
+	/* Sticky menu */
+	$(".navsticky").sticky({
+		topSpacing:0,
+	});
+
+
+    /* Nice Scroll */
+    $("html").niceScroll({
+        cursorwidth: '10px',
+        scrollspeed: '60',
+        mousescrollstep: '45'
+    });
+
+
+});
