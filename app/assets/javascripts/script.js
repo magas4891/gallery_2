c1bb25a725b4c51cab33c437142cfefcefbe314b
magas4891 <magasishe@gmail.com>
2019-09-11 15:29:12 +0300

    updating register form

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -25,4 +25,27 @@ $(document).ready(function(){
         deeplinking: false
     });
 
+    $('ul.nav li.dropdown').hover(function () {
+        $(this).find('.dropdown-menu').stop(true, true);
+        $(this).addClass('open');
+    }, function () {
+        $(this).find('.dropdown-menu').stop(true, true);
+        $(this).removeClass('open');
+    });
+
+    enquire.register("screen and (min-width:992px)", {
+        match : function() {
+
+            $('ul.nav li.dropdown').hover(function () {
+                $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn();
+            }, function () {
+                $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut();
+            });
+        },
+        unmatch : function() {
+
+
+        }
+
+    });
 });
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def user_activity(action)
     Activity.create(user_id: current_user.id, action: action, url: request.original_url)
@@ -14,4 +15,9 @@ class ApplicationController < ActionController::Base
     { locale: I18n.locale }
   end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nick])
+  end
 end
