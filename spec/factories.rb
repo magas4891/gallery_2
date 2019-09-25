a5f819a61a09060fc507d93df84c1574888edd87
magas4891 <magasishe@gmail.com>
2019-09-25 17:07:50 +0300

    tests category - done

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,22 +6,19 @@ FactoryBot.define do
       password_confirmation { "password" }
       name { "Bob" }
     end
-  #
-  #   factory :user_without_email do
-  #     password { "password" }
-  #     password_confirmation { "password" }
-  #     name { "Bob" }
-  #   end
-  #
-  #   factory :user_without_password do
-  #     email { "bob@gmail.com" }
-  #     name { "Bob" }
-  #   end
-  #
-  #   factory :user_without_name do
-  #     email { "bob@gmail.com" }
-  #     password { "password" }
-  #     password_confirmation { "password" }
-  #   end
   end
+  factory :category do
+    factory :valid_category do
+      name { "Category" }
+      description { Faker::Lorem.sentence(6) }
+      user_id { 1 }
+    end
+  end
+  # factory :image do
+  #   factory :valid_image do
+  #     title { "best_picture_ever" }
+  #     category_id { "2" }
+  #     user_id { "1" }
+  #   end
+  # end
 end
---
+++ spec/models/image_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+#
+# RSpec.describe Image, :type => :model do
+#   context 'creation' do
+#     it "is valid with valid attributes" do
+#       expect(image = create(:valid_image)).to be_valid
+#     end
+#     it "is not valid without title"
+#     it "is not valid without category"
+#     it "is not valid without owner"
+#   end
+# end
