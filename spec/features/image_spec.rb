8c60d04cac0a0f209b88ff5b0aa3ca4ce20095cc
magas4891 <magasishe@gmail.com>
2019-10-04 13:47:38 +0300

    f.tests image_spec

--- spec/features/image_spec.rb
+++ spec/features/image_spec.rb
@@ -1,17 +1,37 @@
 require 'spec_helper'
 
-feature 'Category create', driver: :selenium_chrome do
+feature 'Image,', driver: :selenium_chrome do
   feature 'with not signing in user' do
+    given!(:user) { create(:valid_user) }
+    given!(:category) { create(:valid_category) }
+    given!(:image) { create(:valid_image) }
+
+    scenario 'can be show' do
+      visit categories_path
+      visit category_path(:id => category.slug)
+      visit image_path(:id => image.id)
+      expect(page).to have_selector 'img'
+    end
+
+    scenario 'can not leave comment' do
+      visit categories_path
+      visit category_path(:id => category.slug)
+      visit image_path(:id => image.id)
+      expect(page).to have_no_css 'comment_text'
+    end
+
     scenario 'should view form for sign_in' do
       visit categories_path
-      click_button 'ADD NEW CATEGORY'
+      visit category_path(:id => category.slug)
+      click_link 'ADD NEW IMAGE'
       expect(page).to have_content 'You need to sign in or sign up before continuing.'
     end
   end
 
   feature 'with sign_in user' do
-    given(:user) { create(:valid_user) }
-    given(:category) { create(:valid_category) }
+    given!(:user) { create(:valid_user) }
+    given!(:category) { create(:valid_category) }
+    given!(:image) { create(:valid_image) }
     before { visit new_user_session_path
             within '#new_user' do
               fill_in 'user_email', with: user.email
@@ -19,27 +39,26 @@ feature 'Category create', driver: :selenium_chrome do
               click_button 'Login'
             end
             visit categories_path
+            visit category_path(:id => category.slug)
     }
 
     scenario 'should view form adding new category' do
-      click_button 'ADD NEW CATEGORY'
-      expect(page).to have_content 'ADDING NEW CATEGORY'
+      click_link 'ADD NEW IMAGE'
+      expect(page).to have_content 'ADDING NEW IMAGE'
     end
 
     scenario 'after creating should appear new category' do
-      click_button 'ADD NEW CATEGORY'
-      within '#new_category' do
-        fill_in 'category_name', with: category.name
-        fill_in 'category_description', with: category.description
-        click_button 'CREATE'
+      click_link 'ADD NEW IMAGE'
+      within '#new_image' do
+        fill_in 'image_title', with: image.title
+        attach_file 'image_picture', '/home/developer/RoR/gallery_2/spec/support/logo_image.jpg'
+        click_button 'UPLOAD'
       end
-      # print page.html
-      expect(page).to have_content 'Category successful created'
+      expect(page).to have_selector 'img'
     end
 
     scenario 'after creating you can visit new category' do
       visit category_path(:id => category.slug)
-      # print page.html
       expect(page).to have_link 'ADD NEW IMAGE'
     end
 
