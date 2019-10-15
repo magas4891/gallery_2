706ee35fd00287fee48f0018cb95dbe16bcd2ddb
magas4891 <magasishe@gmail.com>
2019-10-15 14:30:19 +0300

    cap production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,26 @@
 # if Rails.env.production?
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'us-east-2'
-  }
-  config.fog_directory = ENV['AWS_BUCKET']
-end
+#   CarrierWave.configure do |config|
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#         provider: 'AWS',
+#         aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#         aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#         region: 'us-east-2'
+#     }
+#     config.fog_directory = ENV['AWS_BUCKET']
+#   end
 # end
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required unless using use_iam_profile
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required unless using use_iam_profile
+      use_iam_profile:       true,                         # optional, defaults to false
+      region:                'us-east-2',                  # optional, defaults to 'us-east-1'
+      # host:                  's3.example.com',             # optional, defaults to nil
+      # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = ENV['AWS_BUCKET']                                     # required
+  config.fog_public     = false                                                 # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+end
